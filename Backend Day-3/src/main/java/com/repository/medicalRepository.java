effca8cc4085a3ac14dd8fa48173c8f80e8c6157
Gugan <110531967+kokkigugan@users.noreply.github.com>
2023-10-05 15:01:09 +0530

    Day 3 done

---
+++ Backend Day-3/src/main/java/com/repository/medicalRepository.java
@@ -0,0 +1,9 @@
+package com.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.entity.medical;
+
+public interface medicalRepository extends JpaRepository<medical,Integer>{
+
+}
