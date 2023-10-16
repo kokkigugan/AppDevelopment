a305d9a0f979e6abcda7df11dcdf32e2d1be6e15
Gugan <110531967+kokkigugan@users.noreply.github.com>
2023-10-16 13:54:36 +0530

    Delete src directory

--- src/main/java/com/youtube/ecommerce/Repository/CategoryRepo.java
+++
@@ -1,9 +0,0 @@
-package com.youtube.ecommerce.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.youtube.ecommerce.model.Category;
-
-public interface CategoryRepo  extends JpaRepository<Category, Long> {
-
-}
